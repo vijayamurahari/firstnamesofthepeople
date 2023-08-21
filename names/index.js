7c453b2cf1ed7eca33aa65e88df5855fe1b995ed
vijaya <vijayamurahari@gmail.com>
2023-08-21 10:08:23 +0000

    firstnamesofthepeople

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const peopleNames=require("../country,state,city,index");
+const get FirstNames=require("..utilities,utils,index");
+const getPeopleInCity=(peopleNames)=> {
+    return getFirstNames(peopleNames);
+};
+module.exports=getPeoplesInCity;
